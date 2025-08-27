44cac39765067181188912bc27afb90788d98535
zeref <abhijeetsingh173+vscode@gmail.com>
2025-08-27 10:22:06 +0530

    lab_04

---
+++ lab-04/01.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+ 
+number=(9 4 7 6 2 5 3)
+
+n=${#number[@]} #length of array
+#bubble
+for ((i=0;i<n;i++))
+    do
+        for((j=0;j<n-i-1;j++))
+        do
+            if [ ${number[j]} -gt ${number[$((j+1))]} ]
+                then
+                    #swap
+                    temp=${number[j]}
+                    number[j]=${number[$((j+1))]}
+                    number[$((j+1))]=$temp
+            fi
+    done
+done
+    
+echo "Sorted array : ${number[@]}"
+        
+
