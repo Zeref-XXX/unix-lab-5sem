03512c1f3e0b096649c2702619523dde7b66c064
zeref <abhijeetsingh173+vscode@gmail.com>
2025-10-15 09:22:18 +0530

    ignore

--- lab-06/02.sh
+++ lab-06/02.sh
@@ -1,4 +1,4 @@
 # Q.1) Create a file input.txt and store the following data in the file
-
+# Q.2 Consider the content of the input.txt file as a record of an employee. Write a shell  command using awk to print the name and salary of each employee.
 awk -F, 'NR>1 {print $1, $5}' input.txt
 
